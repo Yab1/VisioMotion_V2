314351757f8284f03ff5b6cd21031e79eb2366ab
Yab1 <yabilisanu@gmail.com>
2023-08-13 10:50:57 +0300

    Refactor: Used variable for image imports.

---
+++ src/stores/Images.js
@@ -0,0 +1,21 @@
+import quizzical from "../assets/projects/quizzical.png";
+import simpleNotes from "../assets/projects/simpleNotes.png";
+// import techVisions from "";
+// import visioMotion from "";
+// import wayFinderPlus from "";
+// import wayFinderAdmin from "";
+// import wayFinderVR from "";
+// import eCart from "";
+
+const IMAGES = [
+  { id: 0, name: quizzical },
+  { id: 1, name: simpleNotes },
+  // { id: 2, name: techVisions },
+  // { id: 3, name: visioMotion },
+  // { id: 4, name: wayFinderPlus },
+  // { id: 5, name: wayFinderAdmin },
+  // { id: 6, name: wayFinderVR },
+  // { id: 7, name: eCart },
+];
+
+export default IMAGES;
